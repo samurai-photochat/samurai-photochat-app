08ea41f7edb4e203b7664cc0e1b18b49b203f670
VadimKrepskiy <vadim.krepski@gmail.com>
2025-07-18 13:54:16 +0300

    feat(auth): Добавил универсальную компоненту Button

---
+++ shared/styles/ui/button/button.tsx
@@ -0,0 +1,20 @@
+import { ComponentPropsWithoutRef, ElementType, ReactElement, ReactNode } from "react"
+import s from "./button.module.scss"
+
+export type ButtonProps<T extends ElementType = "button"> = {
+  as?: T
+  children?: ReactNode
+  variant?: "primary" | "secondary" | "outlined" | "text"
+  fullWidth?: boolean
+} & ComponentPropsWithoutRef<T>
+
+const Button = <T extends ElementType = "button">(props: ButtonProps<T>) => {
+  const { variant = "primary", fullWidth, className, as: Component = "button", children, ...rest } = props
+  return (
+    <Component className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ""} ${className}`} {...rest}>
+      {children}
+    </Component>
+  )
+}
+
+export default Button as <T extends ElementType = "button">(props: ButtonProps<T>) => ReactElement
