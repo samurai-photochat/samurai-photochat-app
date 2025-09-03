fdbbb79bb095ce27fb818631ea65a98b2a6e315c
VadimKrepskiy <krepskiy.vadim@mail.ru>
2025-09-03 22:43:25 +0300

    feat: Добавил модальное окно создания поста без функционала

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "@/widgets/sidebar/sidebar"
 import s from "./profile.module.css"
+import { CreatePostWindow } from "@/features/posts/ui/CreatePostWindow/CreatePostWindow"
 
 export default function Profile() {
   return (
@@ -7,6 +8,7 @@ export default function Profile() {
       <Sidebar />
       <div className={s.content}>
         <h2>Content</h2>
+        <CreatePostWindow />
       </div>
     </div>
   )
