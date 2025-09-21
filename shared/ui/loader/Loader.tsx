4c2b57de86b1f7fb07cab74bf75cc9d092348d66
yahty01 <yahty01@yandex.ru>
2025-09-21 14:28:13 +0300

    INCTA-15 fix: Обновить Loader

--- shared/ui/loader/Loader.tsx
+++ shared/ui/loader/Loader.tsx
@@ -7,12 +7,13 @@ type LoaderProps = {
   message?: string
   fullScreen?: boolean
   reduced?: boolean
+  embedded?: boolean
 }
 
-export const Loader = ({ message = "", fullScreen = true, reduced = false }: LoaderProps) => {
+export const Loader = ({ message = "", fullScreen = true, reduced, embedded }: LoaderProps) => {
   return (
     <div className={fullScreen ? s.fullscreen : s.inline} role="status" aria-busy="true">
-      <div className={clsx(s.loaderWrapper, reduced && s.reduced)}>
+      <div className={clsx(s.loaderWrapper, reduced && s.reduced, embedded && s.embedded)}>
         <svg className={s.loaderSvg} viewBox="0 0 50 50">
           <defs>
             <linearGradient id="loaderGradient" x1="0%" y1="0%" x2="100%" y2="0%">
@@ -24,7 +25,7 @@ export const Loader = ({ message = "", fullScreen = true, reduced = false }: Loa
           <circle className={s.loaderCircle} cx="25" cy="25" r="20" fill="none" strokeWidth="4" />
         </svg>
       </div>
-      <p className={s.message}>{message}</p>
+      {message && <p className={s.message}>{message}</p>}
     </div>
   )
 }
