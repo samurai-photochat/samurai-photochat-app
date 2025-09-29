f99d65b5a672115519cc415461ba59e2924b4a10
Wlad5 <cebotarivlad814@gmail.com>
2025-09-29 21:37:10 +0200

    INCTA-9 feat(ui): add universal dropdown/context menu

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,7 @@ export default function Home() {
   return (
     <div style={{ display: "flex" }}>
       {isLoggedIn && <Sidebar />}
+      <Sidebar />
       <div>
         <h1>Непобедимые самураи</h1>
 
