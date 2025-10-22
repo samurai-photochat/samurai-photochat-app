ca4489b7da32776aa76169822d71e89727ca57c2
yahty01 <yahty01@yandex.ru>
2025-10-22 18:43:44 +0300

    refactor: Удалены дубликаты - устранена вложенность           lib/hooks           ,           lib/api           ,           lib/redux           Консолидированы хуки -           lib/hooks/useOutsideClick.tsx            →           hooks/           Консолидирован API - lib/api/* →           api/           Консолидирован store - lib/redux/* и model/app/* →           store/           Созданы схемы - lib/signUpSchema/* →           schemas/           Упрощены utils -           lib/random/getRandomNumber.ts            и           utils/localStorage/localStorage.ts            →           utils/           Удалены пустые папки -           lib/            и           model/

---
+++ src/shared/store/index.ts
@@ -0,0 +1,11 @@
+export {
+  appSlice,
+  appReducer,
+  changeTheme,
+  setAppError,
+  setIsLoggedIn,
+  selectThemeMode,
+  selectError,
+  selectIsLoggedIn,
+} from "./appSlice"
+export type { Error } from "./appSlice"
